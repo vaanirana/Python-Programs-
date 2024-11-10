d36cd5d601a09db0db33449ebe4aef7d041b98b4
vaanirana <vaanirana2004@gmail.com>
2024-11-10 15:00:31 +0530

    Update Q21.maths modules.txt.py.py

--- Q21.maths modules.txt.py.py
+++ Q21.maths modules.txt.py.py
@@ -3,4 +3,4 @@ num = 16
 print(f" Square root of {num}:{math.sqrt(num)}")
 print(f"Factorial of {num}:{math.factorial(num)}")
 print (f"Logarithm of {num}:{math.log2(num)}")
-print("THISE PROGRAM IS WRITTEN BY VAANI ERP:- 162")THIS PROGRAM IS WRITTEN BY VAANI, ERP:-162
+print("THISE PROGRAM IS WRITTEN BY VAANI ERP:- 162")
