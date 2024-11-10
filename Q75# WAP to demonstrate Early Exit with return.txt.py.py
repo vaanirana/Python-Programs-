5d4ff3464bc72b4c70ff7f4f2a5756a25da0a77b
vaanirana <vaanirana2004@gmail.com>
2024-11-10 14:59:12 +0530

    Update Q75# WAP to demonstrate Early Exit with return.txt.py.py

--- Q75# WAP to demonstrate Early Exit with return.txt.py.py
+++ Q75# WAP to demonstrate Early Exit with return.txt.py.py
@@ -8,4 +8,4 @@ def find_first_even(numbers):
 nums = [1, 3, 5, 8, 9]
 first_even = find_first_even(nums)
 print(first_even)  # Output: 8
-print("75.THIS PROGRAM IS WRITTEN BY DEEPANSHU, ERP:-005")THIS PROGRAM IS WRITTEN BY VAANI, ERP:-162
+print("THIS PROGRAM IS WRITTEN BY VAANI, ERP:-162")
