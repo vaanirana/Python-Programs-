d7954be413f259fe1e9f322bdec94fbf47b42bae
vaanirana <vaanirana2004@gmail.com>
2024-11-10 14:55:26 +0530

    Update Q87Aliasing Modules and Functions.txt.py.py

--- Q87Aliasing Modules and Functions.txt.py.py
+++ Q87Aliasing Modules and Functions.txt.py.py
@@ -3,4 +3,5 @@ You can give a module or function an alias using the as keyword to shorten the r
 # main.py 
 import math_utils as mu 
 result = mu.add(5, 3) 
-print(result)  # Output: 8THIS PROGRAM IS WRITTEN BY VAANI, ERP:-162
+print(result)  # Output: 8
+print("THIS PROGRAM IS WRITTEN BY VAANI, ERP:-162")
