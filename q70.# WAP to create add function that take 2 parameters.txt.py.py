389d22c43ee47126130ad3dfbacc327363ec91b3
vaanirana <vaanirana2004@gmail.com>
2024-11-10 14:52:34 +0530

    Update q70.# WAP to create add function that take 2 parameters.txt.py.py

--- q70.# WAP to create add function that take 2 parameters.txt.py.py
+++ q70.# WAP to create add function that take 2 parameters.txt.py.py
@@ -3,5 +3,4 @@ def add(a, b):
  return a + b
 result = add(3, 5)
 print(result) # Output: 8
-print("THIS PROGRAM IS WRITTEN BY KAUSHAL, ERP:-154")
-THIS PROGRAM IS WRITTEN BY VAANI, ERP:-162
+print("THIS PROGRAM IS WRITTEN BY VAANI, ERP:-162")
